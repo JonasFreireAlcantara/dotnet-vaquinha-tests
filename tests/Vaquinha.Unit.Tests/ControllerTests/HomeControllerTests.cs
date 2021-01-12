2ba2d6c3b8d823299d6d60f89538f8839b736dd4
Jonas Freire <jonasfreireperson@gmail.com>
2021-01-11 21:02:06 -0300

    Caso de Teste de Unidade - Home Controller
    Adicionado dois casos de teste à classe HomeControllerTests:
      - Verifica função Index;
      - Verifica função Privacy.
    
    O comportamento do método RecuperarDadosIniciaisHomeAsync da classe
    HomeInfoService é emulado utilizando um mock.

--- tests/Vaquinha.Unit.Tests/ControllerTests/HomeControllerTests.cs
+++ tests/Vaquinha.Unit.Tests/ControllerTests/HomeControllerTests.cs
@@ -2,17 +2,66 @@
 using Moq;
 using Vaquinha.MVC.Controllers;
 using Vaquinha.Domain;
+using NToastNotify;
+using Xunit;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Vaquinha.Domain.ViewModels;
+using Vaquinha.Service;
+using AutoMapper;
+using System;
 
 namespace Vaquinha.Unit.Tests.ControllerTests
 {
     public class HomeControllerTests
     {
-        private readonly IHomeInfoService _homeInfoService;
-        private readonly Mock<ILogger<HomeController>> _logger;
+        private readonly Mock<ILogger<HomeController>> _logger = new Mock<ILogger<HomeController>>();
+        private readonly Mock<IToastNotification> _toastNotification = new Mock<IToastNotification>();
+        private readonly Mock<IHomeInfoService> _homeInfoService = new Mock<IHomeInfoService>();
+
+        private HomeController _homeController;
 
         public HomeControllerTests()
         {
+            Task<HomeViewModel> task = Task.FromResult(new HomeViewModel());
+            _homeInfoService.Setup(h => h.RecuperarDadosIniciaisHomeAsync()).Returns(task);
+        }
 
+        [Fact]
+        [Trait("HomeController", "HomeController_Index_RetornaViewCorreta")]
+        public async void HomeController_Index_RetornaViewCorreta()
+        {
+            // Arrange
+            _homeController = new HomeController(_logger.Object, _homeInfoService.Object, _toastNotification.Object);
+
+            // Act
+            var retorno = await _homeController.Index();
+
+            // Assert
+            retorno.Should().BeOfType<ViewResult>();
+            _toastNotification.Verify(t => t.AddErrorToastMessage(It.IsAny<string>(), It.IsAny<LibraryOptions>()), Times.Never);
+
+            var viewResult = (ViewResult) retorno;
+            viewResult.Model.Should().BeOfType<HomeViewModel>();
+        }
+
+        [Fact]
+        [Trait("HomeController", "HomeController_Privacy_RetornaViewCorreta")]
+        public void HomeController_Privacy_RetornaViewCorreta()
+        {
+            // Arrange
+            _homeController = new HomeController(_logger.Object, _homeInfoService.Object, _toastNotification.Object);
+
+            // Act
+            var retorno = _homeController.Privacy();
+
+            // Assert
+            retorno.Should().BeOfType<ViewResult>();
+            _toastNotification.Verify(t => t.AddErrorToastMessage(It.IsAny<string>(), It.IsAny<LibraryOptions>()), Times.Never);
+
+            var viewResult = (ViewResult) retorno;
+            viewResult.Model.Should().BeNull();
         }
     }
 }
